44e16d519d63dd90061c7ab33993856931e686af
Roy Kid <lijichen365@126.com>
2021-01-25 01:09:46 +0800

    add potential module in core

---
+++ test/test_potential.py/test_bondbase.py
@@ -0,0 +1,26 @@
+# author: Roy Kid
+# contact: lijichen365@126.com
+# date: 2021-01-25
+# version: 0.0.1
+from emmm.core.potential.bond_base import BondBase
+import pytest
+
+@pytest.fixture()
+def a_tuple():
+    return 'data'
+
+def test_data(a_tuple):
+    assert a_tuple=='data'
+
+class TestBondBase:
+
+    @pytest.fixture(scope='class')
+    def bondbase(self):
+        bb = BondBase()
+        yield bb
+
+    def test_grid(self, bondbase):
+        bb = bondbase
+        bb.r0 = 1.23
+        rlist = bb._grid()
+        assert len(rlist) == 100
